d0e97ef55629aa9862d88ace49e65e9b609ea94c
wdyprtiwi <widyatiwi00@gmail.com>
2023-11-03 21:11:36 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict'
+const express = require('express')
+const PORT = 5000
+const HOST = '0.0.0.0'
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Hello, world!')
+})
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+app.use(express.static("public"));
+app.listen(port, () => {
+    console.log(`Server berjalan di http://localhost:${port}`);
+});
